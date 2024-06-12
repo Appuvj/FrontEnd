6ef7728c70d9ed161657c550569aaabd4690b9e8
Appuvj <appuvj006@gmail.com>
2024-06-12 18:15:13 +0530

    Add files via upload

---
+++ VIJAY/JavaScript/formAfterSub.js
@@ -0,0 +1,20 @@
+function checkAll(){
+    let ufname=document.myform.fname.value
+    let regfname="^[a-zA-Z]{3,15}$"
+
+    let uedu=document.myform.edu
+    if(ufname.trim()===""){
+        window.alert("this field is required");
+        return false
+    }
+
+    if(!ufname.trim().match(regfname)){
+        window.alert("user name must contain only character min-3 and max-15")
+        return false
+    }
+
+    if(uedu[0].checked==false && uedu[1].checked==false && uedu[2].checked==false &&uedu[3].checked==false && uedu[4].checked==false){
+        window.alert("please select your qualification")
+        return false
+    }
+}
